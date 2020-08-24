7973bf8fcbf28c3cd22fa320aa42976522e5cf9a
A <jesus.e.estrada@hotmail.com>
2020-08-24 17:12:23 -0500

    combined forms into coresponding file names

--- src/components/Login.js
+++ src/components/Login.js
@@ -48,8 +48,43 @@ export default function Login(props) {
   }
 
   return (
-    <div>
-      <LoginForm />
-    </div>
+    <form>
+      <div>
+        <h2>Login</h2>
+      </div>
+
+      <div>
+        <label>Name:&nbsp;
+          <input
+            type='text'
+            name='name'
+            // value={}
+            placeholder='name'
+          // onChange={}
+          />
+        </label>
+
+        <label>Email:&nbsp;
+          <input
+            type='email'
+            name='email'
+            // value={}
+            placeholder='email'
+          // onChange={}
+          />
+        </label>
+
+        <label>Password:&nbsp;
+          <input
+            type='password'
+            name='password'
+            // value={}
+            placeholder='password'
+          // onChange={}
+          />
+        </label>
+
+      </div>
+    </form>
   )
 }
