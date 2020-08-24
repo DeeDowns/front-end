dd0860831724b95c3c846683f2bb1fb2df0af31b
A <jesus.e.estrada@hotmail.com>
2020-08-24 17:10:30 -0500

    re-added Login.js

---
+++ src/components/Login.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
+import { axiosWithAuth } from '../utils/axiosWithAuth'
+
+import axios from 'axios'
+import LoginForm from './LoginForm'
+
+const intitialLoginForm = {
+  name: '',
+  email: '',
+  password: '',
+};
+
+const intitialLoginFormErrors = {
+  name: '',
+  email: '',
+  password: '',
+};
+
+
+export default function Login(props) {
+
+  const [userLogin, setUserLogin] = useState(intitialLoginForm)
+  const [formErrors, setFormErrors] = useState(intitialLoginFormErrors)
+
+  //so we can redirect to register page
+  let history = useHistory()
+
+  //login event handler
+  //the login goes on the onSubmit of the form
+  //wont work as of now without endpoints
+  const login = event => {
+    event.preventDefault()
+    //axiosWithAuth post request to get token from server
+    axiosWithAuth().post(/*'endpoint goes here', login credentials to send*/)
+      .then(res => {
+        console.log(res)
+
+        //send token to local storage
+        // localStorage.setItem('token', res.data.payload)
+
+        //redirect to register page
+        //history.push(route to register '/register')
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  )
+}
