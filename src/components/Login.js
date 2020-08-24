028ef5443ac41456e7cb7607e3f7706543e27c2c
A <jesus.e.estrada@hotmail.com>
2020-08-24 16:30:23 -0500

    login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import LoginForm from './LoginForm'
+
+const intitialLoginForm = {
+  name: '',
+  email: '',
+  password: '',
+};
+
+const intitialLoginFormErrors = {
+  name: '',
+  email: '',
+  password: '',
+};
 
 export default function Login(props) {
+
+  const [userLogin, setUserLogin] = useState(intitialLoginForm)
+  const [formErrors, setFormErrors] = useState(intitialLoginFormErrors)
+
+  const getUserLogin = () => {
+    axios.get('')
+      .then(res => {
+        debugger
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  const postUserLogin = () => {
+    axios.post('')
+      .then(res => {
+        debugger
+      })
+      .catch(err => {
+        console.log(err)
+      })
+      .finally(() => {
+
+      })
+  }
+
+
   return (
-    <>
-    </>
+    <div>
+      <LoginForm />
+    </div>
   )
 }
